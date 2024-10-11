045f5bae340adb3aeb77783734c1f95296955da4
ForsonCN <oprc1939@gmail.com>
2024-10-12 04:50:36 +0800

    Site updated: 2024-10-12 04:50:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/hello-world/"];function toRandomPost(){
+var posts=["2024/10/12/hello-world/","2024/10/12/Hexo 博客美化代码块/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
