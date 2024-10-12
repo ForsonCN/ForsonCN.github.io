b475b60bdb75a38811e3a435120684bdfec4db5b
ForsonCN <oprc1939@gmail.com>
2024-10-12 22:00:32 +0800

    Site updated: 2024-10-12 22:00:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/hello-world/","2024/10/12/Hexo 博客美化代码块/","2024/10/12/hexo01/"];function toRandomPost(){
+var posts=["2024/10/12/hello-world/","2024/10/12/Hexo 博客美化代码块/","2024/10/12/hexo01/","2024/10/12/Hexo基础配置/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
